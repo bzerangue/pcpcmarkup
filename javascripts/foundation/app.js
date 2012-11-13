a29491f66fcda39a1a8e4ac3c4461e9697e62532
Brian Zerangue <brian.zerangue@gmail.com>
2012-11-13 15:41:37 -0600

    uncommented app.js file for IE8 support for block-grid functionality

--- javascripts/foundation/app.js
+++ javascripts/foundation/app.js
@@ -69,10 +69,10 @@
   $('input, textarea').placeholder();
 
   // UNCOMMENT THE LINE YOU WANT BELOW IF YOU WANT IE8 SUPPORT AND ARE USING .block-grids
-  // $('.block-grid.two-up>li:nth-child(2n+1)').css({clear: 'both'});
-  // $('.block-grid.three-up>li:nth-child(3n+1)').css({clear: 'both'});
-  // $('.block-grid.four-up>li:nth-child(4n+1)').css({clear: 'both'});
-  // $('.block-grid.five-up>li:nth-child(5n+1)').css({clear: 'both'});
+  $('.block-grid.two-up>li:nth-child(2n+1)').css({clear: 'both'});
+  $('.block-grid.three-up>li:nth-child(3n+1)').css({clear: 'both'});
+  $('.block-grid.four-up>li:nth-child(4n+1)').css({clear: 'both'});
+  $('.block-grid.five-up>li:nth-child(5n+1)').css({clear: 'both'});
 
   // Hide address bar on mobile devices
   if (Modernizr.touch) {
